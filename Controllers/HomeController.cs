27e62cbe9edf717b98992f34b7f1b892acec3027
wakaflorien <waka.florien45@gmail.com>
2020-01-05 20:49:15 -0800

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationApp.Models;
+
+namespace WebApplicationApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            Home home1= new Home();
+            home1.Regno = 217789999;
+            home1.Name = "waka";
+            return View(home1);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Home home)
+        {
+            if (ModelState.IsValid)
+            {
+                return Content("Success!!");
+            }
+            else
+            {
+                return View(home);
+            }
+        }
+    }
+}
---
+++ Models/Home.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationApp.Models
+{
+    public class Home
+    {
+        [Required(ErrorMessage = "Fill in Registration number!!")]
+        public int Regno { get; set; }
+        [Required(ErrorMessage = "Fill in Your names!!")]
+        public string Name { get; set; }
+    }
+}
